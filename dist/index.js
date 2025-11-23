3da9399440ea4fb08a1e27daf388f8051d5e1d41
sparkingsquirrel@gmail.com <sparkingsquirrel@gmail.com>
2025-11-23 17:23:01 -0600

    2.1 Middleware + sync

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,11 @@
 import express from "express";
+import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponse } from "./api/middleware.js";
 const app = express();
 const PORT = 8080;
+app.use(middlewareLogResponse);
 app.use("/app", express.static("./src/app"));
 app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
-function handlerReadiness(req, res) {
-    res.set("Content-Type", "text/plain");
-    res.send("OK");
-}
