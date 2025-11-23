276ea7ff581c5968f7bb59dabdcd884a8d32c107
sparkingsquirrel@gmail.com <sparkingsquirrel@gmail.com>
2025-11-23 09:48:34 -0600

    1.12 Custom Handlers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+function handlerReadiness(req: Request, res: Response) {
+    res.set("Content-Type", "text/plain");
+    res.send("OK");
+}
