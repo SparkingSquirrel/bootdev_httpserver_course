83df5693f1fc4546c1defb1f27607f0f37e7b090
sparkingsquirrel@gmail.com <sparkingsquirrel@gmail.com>
2025-11-25 09:26:44 -0600

    remove dist dir

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@ const PORT = 8080;
 app.use(middlewareLogResponse);
 app.use("/app", middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
-app.use("/metrics", handleGetFileserverHits);
-app.use("/reset", handleResetFileserverHits);
-app.get("/healthz", handlerReadiness);
+app.use("/api/metrics", handleGetFileserverHits);
+app.use("/api/reset", handleResetFileserverHits);
+app.get("/api/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
