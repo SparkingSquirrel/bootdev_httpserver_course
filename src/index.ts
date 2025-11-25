13cd0a7dec05045b81ea94983fe242897a83b7e7
sparkingsquirrel@gmail.com <sparkingsquirrel@gmail.com>
2025-11-25 11:08:37 -0600

    4.3 json middleware and sync

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
   middlewareLogResponse,
   middlewareMetricsInc,
 } from "./api/middleware.js";
+import { handlerChirpsValidate } from "./api/chirps.js";
 
 const app = express();
 const PORT = 8080;
@@ -16,7 +17,9 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerMetrics);
-app.get("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
+
+app.post("/api/validate_chirp", handlerChirpsValidate);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
